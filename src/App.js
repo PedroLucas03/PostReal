0c92a4962e2ffbe0d5a807973cf879d47cd612c7
Alunos <engs-pedrolubina@camporeal.edu.br>
2024-08-21 22:22:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect } from 'react';
+import {db, auth} from './firebaseConnection';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  doc,
+  setDoc,
+  collection, 
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
 
-export default App;
+} from 'firebase/firestore'
+
+import{
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+} from 'firebase/auth'
+
+function App(){
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+  const [idPost, setIdPost] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [post, setPost] = useState([]);
+
+
+  useEffect(() => {
+    async function carregarPosts(){
+      const dados = onSnapshot(collection(db,"posts"), (snapshot) => {
+
+        let listaPost = [];
+        snapshot.forEach((doc)=>{
+          listaPost.push(
+            {
+              id: doc.id, 
+              titulo: doc.data().titulo,
+              autor: doc.data().autor
+            }
+          );
+          
+    
+        });
+        setPost(listaPost)
+      })
+    }
+    carregarPosts();
+  },[])
+
+    //C - create
+    async function adicionarPosts(){
+      await addDoc(collection(db,"posts"),{
+        titulo: titulo,
+        autor: autor,
+      }).then(()=>{
+        alert("Cadastrado realizado com sucesso!")
+        setAutor('');
+        setTitulo('');
+      }).catch((error) =>{
+        console.log(error);
+      })
+    }
+
+    //R - read
+    async function buscarPosts(){
+      const config = collection(db, "posts");
+      await getDocs(config).then((snapshot)=>{
+        let lista = [];
+
+        snapshot.forEach((doc)=>{
+          lista.push(
+            {
+              id: doc.id, 
+              titulo: doc.data().titulo,
+              autor: doc.data().autor
+            }
+          );
+        });
+
+        setPost(lista);
+
+      }).catch((error) =>{
+        console.log(error);
+      })
+    }
+
+    //U - update
+    async function editarPost(){
+      const postEditado = doc(db, "post", idPost);
+
+      await updateDoc(postEditado, {
+        titulo: titulo, 
+        autor: autor
+      }).then(()=>{
+        alert("Post editado com sucesso!");
+        setIdPost('');
+        setTitulo('');
+        setAutor('');
+      }).catch((error)=> {
+        console.log(error);
+      })
+    }
+
+    //D - delete
+    async function excluirPost(id){
+      const postDeletado = doc(db, "post", id);
+      await deleteDoc(postDeletado)
+      .then(()=>{alert("Post deletado com sucesso!")})
+      .catch((error) =>{
+        console.log(error);
+      })
+    }
+  }
